d95691b89585bfee6c888a566db80bc0427b1c90
Tomasz Król <tomicode@gmail.com>
2019-10-02 18:33:01 +0200

    Basic views stucture.

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,6 @@
         <span>Vuetify</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
